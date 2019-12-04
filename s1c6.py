4089f1ba94882453b07c7ad37f22f7f1818aba4f
Rob C <robert@robertjcoleman.com>
2019-12-04 11:11:10 -0500

    Challenge 6 is broken, to fix

--- s1c6.py
+++ s1c6.py
@@ -1,73 +1,51 @@
 import convert
+import cipher
+import scoreboard
 
-
-def rawHammingDistance(byte1, byte2):
-    total = 0
-    for a,b in zip(byte1, byte2):
-        total += bin(a^b).count("1")
-    return total
-
-def hammingDistance(string1, string2):
-    total = 0
-    for a,b in zip(string1, string2):
-        total += bin(ord(a)^ord(b)).count("1")
-    return total    
 
 def createArrays(block, size):
     array = []
     count = 0
-    temp = bytearray()
+    temp = b""
     for byte in block:
         count += 1
-        temp.append(byte)
+        temp += bytes([byte])
         if count == size:
             count = 0
             array.append(temp)
-            temp = bytearray()
+            temp = b""
     return array
+
 
 def transpose(array):
     outside = len(array)
     inside = len(array[0])
     print(outside, inside)
     tarray = []
-    temp = bytearray()
+    temp = b""
     for i in range(inside):
-        for j in range(outside): 
-            temp.append(array[j][i])
+        for j in range(outside):
+            temp +=  bytes([array[j][i]])
         tarray.append(temp)
-        temp = bytearray()
+        temp = b""
     return tarray
 
-if __name__ == "__main__":
-    string1 = "this is a test"
-    string2 = "wokka wokka!!!"
 
-    answer = hammingDistance(string1, string2)
-    print("First test: ", answer == 37, answer, "\n")
-    
+if __name__ == "__main__":
     block = ""
     with open("6.txt", "r") as theBlock:
         block = convert.base64_to_byte_array(theBlock.read())
-    total = 4
-    
-    topDist = [-1000] * total
-    topSize = [-1000] * total
-    best = -1000
+
+    board = scoreboard.Scoreboard(4, ["Key Size"], -1000)
 
     for key in range(2, 41):
-        dist = rawHammingDistance(block[:key], block[key:2*key])
-        dist2 = rawHammingDistance(block[2*key:3*key], block[3*key:4*key])
-        avg = -((dist)/ (key))
-        if avg > best:
-            index = s1c3.updateTopList(topDist, avg)    
-            s1c3.insertTopList(topSize, key, index)
-            best = topDist[-1]    
-    for i in range(4):
-        print(i+1, "th Place", "\n| Distance: ", -topDist[i], "\n| KeySize: ", topSize[i], "\n\n---------") 
+        dist = cipher.hamming_distance_of_bytes(block[:key], block[key:2 * key])
+        dist2 = cipher.hamming_distance_of_bytes(block[2 * key:3 * key], block[3 * key:4 * key])
+        avg = -(dist / (2 * key))
+        board.update([avg], [[key]])
+
+    print(board)
 
     arrays = transpose(createArrays(block, 2))
     for series in arrays:
-        a,b,c = s1c3.findCharacter(series, 3)
-        print(a,c)
-        print("----------------------------")
+        cipher.single_byte_xor_in_iterable(series, 3)
